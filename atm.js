ecd70c8f67ce37792ac7f9e6b8c708f9281715d3
Olukayode <olukayodeshokunbi@gmail.com>
2021-01-27 20:13:40 -0500

    Created initial project files

---
+++ atm.js
@@ -0,0 +1,7 @@
+function getBalance() {}
+
+function withdraw() {}
+
+function deposit() {}
+
+function validatePin() {}
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World, I'm learning Node");
+const prompt = require("prompt-sync")();
+
+console.log("Coffee or Tea");
+const drinkChoice = prompt(`What drink will you like to have?`);
+
+console.log(`${drinkChoice} for me please!`);
