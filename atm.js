80acaa442c64a208c40bde0bdf6084ed3d58e40f
Olukayode <olukayodeshokunbi@gmail.com>
2021-01-27 21:31:42 -0500

    Completed PIN validation function

--- atm.js
+++ atm.js
@@ -1,12 +1,24 @@
+const prompt = require("prompt-sync")();
 const account = require("./account");
 
+// function to get balance
 function getBalance() {}
 
+// function for withdrawals
 function withdraw() {}
 
+// function to deposit
 function deposit() {}
 
-function validatePin() {}
+// function to request and valid pin
+function validatePin() {
+  let enteredPin = Number(prompt(`Enter your PIN to continue`));
+  if (enteredPin === account.pin) {
+    console.log(`Success!`);
+  } else if (enteredPin !== account.pin) {
+    validatePin();
+  }
+}
 
 module.exports = {
   balance: getBalance,
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const prompt = require("prompt-sync")();
 const atm = require("./atm");
+
+const bankName = "devOlu";
+
+console.log(`Welcome to ${bankName} ATM Terminal`);
+
+// Call validate pin functiono
+atm.valid();
