c5a506cc9f31554ddc71e9826714ff97693ec146
Olukayode <olukayodeshokunbi@gmail.com>
2021-01-28 06:11:17 -0500

    Completed functionality to check balance, withdraw, and make deposit

--- atm.js
+++ atm.js
@@ -2,17 +2,48 @@ const prompt = require("prompt-sync")();
 const account = require("./account");
 
 // function to get balance
-function getBalance() {}
+function getBalance() {
+  prompt(`Your account balance is: $${account.balance}`);
+  operationToPerform();
+}
 
 // function for withdrawals
-function withdraw() {}
+function withdraw(currentBalance) {
+  let amountToWithdraw = Number(
+    prompt(`How much would you like to withdraw today?\n$`)
+  );
+  if (amountToWithdraw > currentBalance) {
+    console.log(
+      `You do not have sufficent funds to withdraw this amount.\nPlease  enter a lesser amount:\n`
+    );
+    withdraw(currentBalance);
+  } else {
+    currentBalance -= amountToWithdraw;
+    account.balance = currentBalance;
+    console.log(`Please take your cash!`);
+    console.log(`You new balance is $${account.balance}\n`);
+  }
+}
 
 // function to deposit
-function deposit() {}
+function deposit(currentBalance) {
+  let amountToDeposit = Number(
+    prompt(`Enter the amount you like to deposit today\n$`)
+  );
+  if (amountToDeposit <= 0) {
+    console.log(`Please enter an amount greater than $0.00`);
+    deposit(currentBalance);
+  } else {
+    currentBalance += amountToDeposit;
+    account.balance = currentBalance;
+    console.log(`Transaction succesful`);
+    console.log(`You new balance is $${account.balance}\n`);
+  }
+}
 
 // function to request and valid pin
 function validatePin() {
-  let enteredPin = Number(prompt(`Enter your PIN to continue`));
+  let enteredPin = Number(prompt(`Enter your PIN to continue\n`));
   if (enteredPin === account.pin) {
     console.log(`Success!`);
   } else if (enteredPin !== account.pin) {
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const prompt = require("prompt-sync")();
+const account = require("./account");
 const atm = require("./atm");
 
 const bankName = "devOlu";
@@ -7,3 +8,38 @@ console.log(`Welcome to ${bankName} ATM Terminal`);
 
 // Call validate pin functiono
 atm.valid();
+
+function operationToPerform() {
+  let choice = Number(
+    prompt(
+      `What operation would you like to perform today?\n\nEnter:\n1 to Check Balance\n2 to Withdraw\n3 to Deposit\n`
+    )
+  );
+  return choice;
+}
+
+//users choice stored in CHOICE variable
+let choice = operationToPerform();
+
+function performOperation(choice) {
+  switch (choice) {
+    case 1:
+      //get balance
+      atm.balance();
+      break;
+    case 2:
+      //withdraw
+      atm.withdraw(account.balance);
+      break;
+    case 3:
+      //deposit
+      atm.deposit(account.balance);
+      break;
+    case 4:
+    //exit
+    default:
+      operationToPerform();
+  }
+}
+
+performOperation(choice);
